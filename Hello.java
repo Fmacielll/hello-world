14003b247197a6fa9958514308792c776bcccbcc
Fmacielll <ffrmaciel@minha.fag.edu.br>
2025-03-12 19:52:39 -0300

    Adicionado método ola mundo()

--- Hello.java
+++ Hello.java
@@ -1,6 +1,10 @@
 public class Hello {
+    public static void olaMundo() {
+        System.out.println("Ola Mundo");
+    }
+
     public static void main(String[] args) {
         System.out.println("Hello World");
+        olaMundo();
     }
 }
-
